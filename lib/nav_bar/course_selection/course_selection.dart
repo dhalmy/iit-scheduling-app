c81d345344fd1930c5ef15a1d0996de8f72ec36e
anthonyfranco0123 <anthonyfranco0123@gmail.com>
2023-10-16 18:59:46 -0500

    Minor changes to supabase data print.

--- lib/nav_bar/course_selection/course_selection.dart
+++ lib/nav_bar/course_selection/course_selection.dart
@@ -20,10 +20,10 @@ class _CourseSelectionState extends State<CourseSelection> {
   void initState() {
     super.initState();
 
-    // _fetchData();
-    sortCourseSubjectsAlphabetically(courseSubjects);
-    final courseSubjectsByArea = groupCourseSubjectsByArea(courseSubjects);
-    displayCourseSubjects(courseSubjectsByArea);
+    _fetchData();
+    // sortCourseSubjectsAlphabetically(courseSubjects);
+    // final courseSubjectsByArea = groupCourseSubjectsByArea(courseSubjects);
+    // displayCourseSubjects(courseSubjectsByArea);
   }
 
   Future<void> _fetchData() async {
@@ -35,49 +35,84 @@ class _CourseSelectionState extends State<CourseSelection> {
     }
   }
 
-// Sort the list of course subjects alphabetically.
-  void sortCourseSubjectsAlphabetically(List<Map<String, String>> courseSubjects) {
-    courseSubjects.sort((a, b) => a['courseSubject']!.compareTo(b['courseSubject']!));
-  }
+  // Future<void> _fetchData() async {
+  //   try {
+  //     final data = await Supabase.instance.client.from('fall2023').select('courseSubject');
+  //
+  //     // Sort the course subjects alphabetically.
+  //     _courseSubjects = data.data.map((e) => e['courseSubject']).toList();
+  //     _courseSubjects.sort((a, b) => a.compareTo(b));
+  //
+  //     // Display the course subjects.
+  //     setState(() {});
+  //   } catch (error) {
+  //     // Handle error
+  //   }
+  // }
 
-// Group the list of course subjects by subject area.
-  Map<String, List<Map<String, String>>> groupCourseSubjectsByArea(
-      List<Map<String, String>> courseSubjects) {
-    final Map<String, List<Map<String, String>>> courseSubjectsByArea = {};
-    for (final courseSubject in courseSubjects) {
-      final subjectArea = courseSubject['courseSubject']![0];
-      if (!courseSubjectsByArea.containsKey(subjectArea)) {
-        courseSubjectsByArea[subjectArea] = [];
-      }
-      courseSubjectsByArea[subjectArea]!.add(courseSubject);
-    }
-    return courseSubjectsByArea;
-  }
+  // Future<void> _fetchData() async {
+  //   try {
+  //     final data = await Supabase.instance.client.from('fall2023').select('courseSubject');
+  //
+  //     // Sort the course subjects alphabetically.
+  //     _courseSubjects = data.data.map((e) => e['courseSubject']).toList();
+  //     _courseSubjects.sort((a, b) => a.compareTo(b));
+  //
+  //     // Display the sorted course subjects.
+  //     print('Course subjects (sorted alphabetically):');
+  //     for (final courseSubject in _courseSubjects) {
+  //       print("Printed!");
+  //       print(courseSubject);
+  //     }
+  //   } catch (error) {
+  //     // Handle error
+  //     print("Failed");
+  //   }
+  // }
 
-// Display the list of course subjects in a more readable format.
-  void displayCourseSubjects(Map<String, List<Map<String, String>>> courseSubjectsByArea) {
-    print('Course Subjects by Area:');
-    for (final subjectArea in courseSubjectsByArea.keys) {
-      print('  - $subjectArea:');
-      for (final courseSubject in courseSubjectsByArea[subjectArea]!) {
-        print('    - ${courseSubject['courseSubject']}');
-      }
-    }
-  }
-
-// Example usage:
-
-  final courseSubjects = [
-    {'courseSubject': 'AAH'},
-    {'courseSubject': 'ARCH'},
-    {'courseSubject': 'AS'},
-    {'courseSubject': 'BIOL'},
-    {'courseSubject': 'BME'},
-    {'courseSubject': 'BUS'},
-    {'courseSubject': 'CAE'},
-    {'courseSubject': 'CHE'},
-    {'courseSubject': 'CIV'},
-  ];
+// // Sort the list of course subjects alphabetically.
+//   void sortCourseSubjectsAlphabetically(List<Map<String, String>> courseSubjects) {
+//     courseSubjects.sort((a, b) => a['courseSubject']!.compareTo(b['courseSubject']!));
+//   }
+//
+// // Group the list of course subjects by subject area.
+//   Map<String, List<Map<String, String>>> groupCourseSubjectsByArea(
+//       List<Map<String, String>> courseSubjects) {
+//     final Map<String, List<Map<String, String>>> courseSubjectsByArea = {};
+//     for (final courseSubject in courseSubjects) {
+//       final subjectArea = courseSubject['courseSubject']![0];
+//       if (!courseSubjectsByArea.containsKey(subjectArea)) {
+//         courseSubjectsByArea[subjectArea] = [];
+//       }
+//       courseSubjectsByArea[subjectArea]!.add(courseSubject);
+//     }
+//     return courseSubjectsByArea;
+//   }
+//
+// // Display the list of course subjects in a more readable format.
+//   void displayCourseSubjects(Map<String, List<Map<String, String>>> courseSubjectsByArea) {
+//     print('Course Subjects by Area:');
+//     for (final subjectArea in courseSubjectsByArea.keys) {
+//       print('  - $subjectArea:');
+//       for (final courseSubject in courseSubjectsByArea[subjectArea]!) {
+//         print('    - ${courseSubject['courseSubject']}');
+//       }
+//     }
+//   }
+//
+// // Example usage:
+//
+//   final courseSubjects = [
+//     {'courseSubject': 'AAH'},
+//     {'courseSubject': 'ARCH'},
+//     {'courseSubject': 'AS'},
+//     {'courseSubject': 'BIOL'},
+//     {'courseSubject': 'BME'},
+//     {'courseSubject': 'BUS'},
+//     {'courseSubject': 'CAE'},
+//     {'courseSubject': 'CHE'},
+//     {'courseSubject': 'CIV'},
+//   ];
 
 
   @override
