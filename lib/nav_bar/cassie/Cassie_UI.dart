188db26731752df2d96f44fec1b7fee8238788ec
Mosa-code <Mosaabdelnabi@gmail.com>
2023-10-23 12:20:56 -0500

    Created a UI for CASSIE

---
+++ lib/nav_bar/cassie/Cassie_UI.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: text,
+      isUser: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+
+    // Replace this with the logic to send the user's message to your AI model
+    // and get a response.
+    String response = "AI response goes here";
+
+    ChatMessage botMessage = ChatMessage(
+      text: response,
+      isUser: false,
+    );
+    setState(() {
+      _messages.insert(0, botMessage);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AI Chat"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              reverse: true,
+              itemCount: _messages.length,
+              itemBuilder: (_, int index) => _messages[index],
+            ),
+          ),
+          Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).canvasColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: const InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () => _handleSubmitted(_textController.text),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({required this.text, required this.isUser});
+
+  final String text;
+  final bool isUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          isUser
+              ? Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: const CircleAvatar(
+              child: Text("User"),
+            ),
+          )
+              : Container(),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: isUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  isUser ? "You" : "AI",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text(text),
+                ),
+              ],
+            ),
+          ),
+          isUser
+              ? Container()
+              : Container(
+            margin: const EdgeInsets.only(left: 16.0),
+            child: const CircleAvatar(
+              child: Text("AI"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
