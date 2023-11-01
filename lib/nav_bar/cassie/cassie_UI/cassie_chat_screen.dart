722f5f7490a9232b6c863f2b986d6994000776fc
Mosa-code <Mosaabdelnabi@gmail.com>
2023-10-31 19:03:36 -0500

    Removed another appbar I forgot about, and then I seperated the Cassie_UI.dart into two files for convinience, one is cassie_chat_message.dart which displays the message from the AI, the other is cassie_chat_screen.dart which just displays the screen.

---
+++ lib/nav_bar/cassie/cassie_UI/cassie_chat_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'cassie_chat_message.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({required Key key}) : super(key: key);
+
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: text,
+      isUser: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+
+    // Replace this with the logic to send the user's message to your AI model
+    // and get a response.
+    String response = "AI response goes here";
+
+    ChatMessage botMessage = ChatMessage(
+      text: response,
+      isUser: false,
+    );
+    setState(() {
+      _messages.insert(0, botMessage);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+          color: Theme.of(context).primaryColor,
+          child: const Center(
+            child: Text(
+              "AI Chat",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemCount: _messages.length,
+                  itemBuilder: (_, int index) => _messages[index],
+                ),
+              ),
+              Divider(height: 1.0),
+              Container(
+                decoration: BoxDecoration(color: Theme.of(context).cardColor),
+                child: _buildTextComposer(),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).canvasColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: const InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () => _handleSubmitted(_textController.text),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
