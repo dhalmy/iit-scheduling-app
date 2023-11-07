44eaaff7eb22e2598ff923936e6a372c1f94bf57
anthonyfranco0123 <anthonyfranco0123@gmail.com>
2023-11-06 18:32:30 -0600

    Using provider to add courses to a list.

--- lib/nav_bar/course_selection/selected_courses.dart
+++ lib/nav_bar/course_selection/selected_courses.dart
@@ -1,56 +1,27 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-class SelectedCourses with ChangeNotifier {
-  static SelectedCourses? _instance;
-
-  SelectedCourses(); // Named constructor
-
-  static SelectedCourses? get instance {
-    _instance ??= SelectedCourses._();
-    return _instance;
-  }
-
-  SelectedCourses._();
-
+class SelectedCourses extends ChangeNotifier {
   final List<String> _courses = [];
 
   void addCourse(String course) {
-    // Check if the course code or name is empty.
-    if (course.isEmpty) {
+    if (course.isEmpty || _courses.contains(course)) {
       return;
     }
-
-    // Check if the course code or name already exists in the list.
-    if (_courses.contains(course)) {
-      return;
-    }
-
-    // Add the course to the list.
     _courses.add(course);
+    notifyListeners();
   }
 
   void removeCourse(String course) {
-    // Check if the course code or name is in the list.
-    if (!_courses.contains(course)) {
-      return;
-    }
-
-    // Remove the course from the list.
     _courses.remove(course);
+    notifyListeners();
   }
 
   void clearCourses() {
-    // Clear the list of selected courses.
     _courses.clear();
+    notifyListeners();
   }
 
   List<String> get courses {
     return _courses;
-  }
-
-  // Implement the notifyListeners method to notify listeners of changes
-  @override
-  void notifyListeners() {
-    super.notifyListeners();
   }
 }
